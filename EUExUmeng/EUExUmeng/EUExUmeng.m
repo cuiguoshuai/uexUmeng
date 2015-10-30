47d87de108839633e3471c7160e4f606e8edd0e4
JinHvung <411376240@qq.com>
2015-10-30 17:43:06 +0800

    修改plist于安卓一致

--- EUExUmeng/EUExUmeng/EUExUmeng.m
+++ EUExUmeng/EUExUmeng/EUExUmeng.m
@@ -22,7 +22,7 @@
 //      NSString* startWithAppkey=@"562df76b67e58e0592003544";
 //      NSString* channelId=@"web";
 + (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
-    NSString *startWithAppkey=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengAppkey"];
+    NSString *startWithAppkey=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengAppKey"];
     NSString *channelId=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengChannelId"];
     [MobClick startWithAppkey:startWithAppkey reportPolicy:BATCH   channelId:channelId];
     return YES;
