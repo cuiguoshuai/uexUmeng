27ceaae60b269be2f7ab12efa77b08834a76b267
connyliu <21165491@qq.com>
2015-10-30 11:10:40 +0800

    APP启动时就会调用初始化

--- EUExUmeng/EUExUmeng/EUExUmeng.m
+++ EUExUmeng/EUExUmeng/EUExUmeng.m
@@ -14,38 +14,40 @@
 
 -(id)initWithBrwView:(EBrowserView *) eInBrwView {
     if (self = [super initWithBrwView:eInBrwView]) {
-        NSString *startWithAppkey=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengAppkey"];
-        NSString *channelId=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengChannelId"];
-        
-        //测试Key
-//        NSString* startWithAppkey=@"562df76b67e58e0592003544";
-//        NSString* channelId=@"web";
-        [MobClick startWithAppkey:startWithAppkey reportPolicy:BATCH   channelId:channelId];
+
     }
     return self;
+}
+//      测试Key
+//      NSString* startWithAppkey=@"562df76b67e58e0592003544";
+//      NSString* channelId=@"web";
++ (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
+    NSString *startWithAppkey=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengAppkey"];
+    NSString *channelId=[[[NSBundle mainBundle] infoDictionary] objectForKey:@"uexUmengChannelId"];
+    [MobClick startWithAppkey:startWithAppkey reportPolicy:BATCH   channelId:channelId];
+    return YES;
 }
 
 -(void)onEvent:(NSMutableArray*)inArguments{
     if(inArguments.count < 2){
         return;
     }
-    NSString* evevtId=inArguments[0];
+    NSString* eventId=inArguments[0];
     NSDictionary* attributes= [inArguments[1] JSONValue];
-    [MobClick event:evevtId attributes:attributes];
+    if(![eventId isKindOfClass:[NSString class]]||![attributes isKindOfClass:[NSDictionary class]]){
+        return;
+    }
+    [MobClick event:eventId attributes:attributes];
 }
 
 -(void)getDeviceInfo:(NSMutableArray*)inArguments{
     Class cls = NSClassFromString(@"UMANUtil");
     SEL deviceIDSelector = @selector(openUDIDString);
-    NSString *deviceID = nil;
+    NSString *deviceID = @"";
     if(cls && [cls respondsToSelector:deviceIDSelector]){
         deviceID = [cls performSelector:deviceIDSelector];
     }
-//    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:@{@"oid" : deviceID}
-//                                                       options:NSJSONWritingPrettyPrinted
-//                                                         error:nil];
-//    
-//    NSLog(@"%@", [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding]);
+
     [self cbDeviceInfo:[@{@"oid":deviceID} JSONFragment]];
 }
 
