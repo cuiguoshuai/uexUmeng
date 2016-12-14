1324499eb44e54c1994fcdf190a6fbbf40550a06
JinHvung <411376240@qq.com>
2016-12-14 15:04:41 +0800

    update from 4.0

--- EUExUmeng/EUExUmeng/EUExUmeng.m
+++ EUExUmeng/EUExUmeng/EUExUmeng.m
@@ -8,16 +8,13 @@
 
 #import "EUExUmeng.h"
 #import <UMMobClick/MobClick.h>
+<<<<<<< HEAD
 #import "JSON/JSON.h"
+=======
+>>>>>>> origin/dev-4.0
+
 
 @implementation EUExUmeng
-
--(id)initWithBrwView:(EBrowserView *) eInBrwView {
-    if (self = [super initWithBrwView:eInBrwView]) {
-
-    }
-    return self;
-}
 //      测试Key
 //      NSString* startWithAppkey=@"562df76b67e58e0592003544";
 //      NSString* channelId=@"web";
@@ -34,13 +31,9 @@
     if(inArguments.count < 1){
         return;
     }
-    NSString* eventId=inArguments[0];
-    id attributes;
-    if(inArguments.count>1){
-        attributes= [inArguments[1] JSONValue];
-    }
-    if([eventId isKindOfClass:[NSString class]]){
-        if([attributes isKindOfClass:[NSDictionary class]]){
+    ACArgsUnpack(NSString* eventId,NSDictionary*attributes) = inArguments;
+    if(eventId){
+        if(attributes ){
             [MobClick event:eventId attributes:attributes];
         }
         else{
@@ -49,20 +42,22 @@
     }
 }
 
--(void)getDeviceInfo:(NSMutableArray*)inArguments{
+-(NSDictionary*)getDeviceInfo:(NSMutableArray*)inArguments{
     Class cls = NSClassFromString(@"UMANUtil");
     SEL deviceIDSelector = @selector(openUDIDString);
-    NSString *deviceID = @"";
+    NSString *deviceID = nil;
     if(cls && [cls respondsToSelector:deviceIDSelector]){
         deviceID = [cls performSelector:deviceIDSelector];
     }
-
-    [self cbDeviceInfo:[@{@"oid":deviceID} JSONFragment]];
-}
-
-//callback
--(void)cbDeviceInfo:(NSString*)param{
-    NSString *cbStr=[NSString stringWithFormat:@"if(uexUmeng.cbGetDeviceInfo != null){uexUmeng.cbGetDeviceInfo('%@');}",param];
-    [EUtility brwView:meBrwView evaluateScript:cbStr];
+    
+    if (deviceID) {
+        [self.webViewEngine callbackWithFunctionKeyPath:@"uexUmeng.cbGetDeviceInfo" arguments:ACArgsPack([@{@"oid":deviceID} ac_JSONFragment])];
+        return @{@"device_id":deviceID};
+        
+    }else{
+        [self.webViewEngine callbackWithFunctionKeyPath:@"uexUmeng.cbGetDeviceInfo" arguments:ACArgsPack([@{@"oid":@""} ac_JSONFragment])];
+        return nil;
+    }
+    
 }
 @end
